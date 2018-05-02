bf4e83781e29b8f665cc530678157a3fcd58df02
SuperY <535720869@qq.com>
2018-05-02 15:02:19 +0800

    dispatch msg
    dispatch msg

--- script/router/impl.lua
+++ script/router/impl.lua
@@ -1,4 +1,20 @@
-function excute(msg)
+local sendMsgToSid = sendMsgToSid
+
+function sendMsg(msg)
+	sendMsgToSid(msg.sid, msg.ms)
+end
+
+local Funcs = {
+	sendMsgToSid = sendMsgToSid,
+}
+
+function excute(m)
+	local msg = table:unserialize(m.msg)
+	local fName = msg.fName
+	local sid   = msg.sid
+	local func = Funcs[fName]
+	assert(func)
+	rpcco:excuteC(nil, func, msg)
 end
 
 function tick()
