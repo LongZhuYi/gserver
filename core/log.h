569ce0c134447a6be9916d579c5a66ef0ee289ea
SuperY <535720869@qq.com>
2018-05-04 17:17:46 +0800

    log
    log

--- core/log.h
+++ core/log.h
@@ -1,26 +1,30 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
+
 #define N 1
 #define W 2
 #define E 3
-
-/*void logHandler(int, char*, ...)
-
-#define LOG(fa, ...) logHandler(N, fa, ...)
-#define WARRING(fa, ...) logHandler(W, fa, ...)
-#define ERROR(fa, ...) logHandler(E, fa, ...)
-
 #define MAX_LINE  1024
-static char* buff = (char*) malloc(MAX_LINE);
 
-void logHandler(int level, char* fa, ...){
-	va_list ap;
-	va_start(ap, fmt);
-	//int n=vsnprintf(buffer+1,LogMax,fmt,ap);
-	snprintf(buff, MAX_LINE, fa, ap);
-	va_end(ap);
-	printf("%s\n",buff);
-}*/
+void logHandler(int, char*, ...);
 
+#define LOG(fa, args...) logHandler(N, fa, ##args)
+#define WARRING(fa, args...) logHandler(W, fa, ##args)
+#define ERROR(fa, args...) logHandler(E, fa, ##args)
+
+
+
+void logHandler(int level, const char* format, ...){
+	char buffer[MAX_LINE];
+	va_list args;
+	va_start(args, format);
+	vsnprintf (buffer,MAX_LINE,format, args);
+	//perror (buffer);
+	printf("%s\n", buffer);
+	va_end (args);
+}
 #endif
